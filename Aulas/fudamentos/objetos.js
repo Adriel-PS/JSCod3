8e499cdca4352d926796c68a043fa1695146dc1f
Adriel Siqueira <adriel@taqe.com.br>
2021-08-13 00:34:22 -0300

    aulas do dia 12/08

---
+++ Aulas/fudamentos/objetos.js
@@ -0,0 +1,16 @@
+const prod1 = {}
+prod1.nome="celular ultra mega"
+prod1.preço=4459.99
+
+
+console.log(prod1)
+
+const prod2 ={
+    nome:'camisa polo',
+    preço:'79.90',
+    codig :{
+        id : 123124561
+    }
+}
+
+console.log(prod2)
